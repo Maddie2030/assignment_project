ed73b71f6a6a202b0efcdd8e55142522337f3923
Maddie2030 <madhurian2030@gmail.com>
2025-05-15 19:06:39 +0530

    added port expose in app.py file

--- app/app.py
+++ app/app.py
@@ -17,6 +17,5 @@ def hello_world():
     '..............................,,,,,,,,,...</p>'
 
 
-if __name__ == '__main__':
-  
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080, debug="True")
